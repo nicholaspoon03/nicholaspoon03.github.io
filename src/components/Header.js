9ee6b30583379b93978c415c47fbda812c591860
nicholaspoon03 <npoon2003@gmail.com>
2025-06-29 14:31:28 -0400

    Adding selected state colour in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "../App.css";
 import Home from "../pages/Home";
 
 function Header() {
+    const location = useLocation()
+
     return (
         <header className="header">
             <Link to="/" className="header-links">
@@ -10,10 +12,10 @@ function Header() {
             </Link>
             
             <nav className="header-links-row">
-                <Link to="/" className="header-links">Home</Link>
-                <Link to="/experience" className="header-links">Experience</Link>
-                <Link to="/education" className="header-links">Education</Link>
-                <Link to="/projects" className="header-links">Projects</Link>
+                <Link to="/" className={`header-links ${location.pathname === "/" ? "active-header" : ""}`}>Home</Link>
+                <Link to="/experience" className={`header-links ${location.pathname === "/experience" ? "active-header" : ""}`}>Experience</Link>
+                <Link to="/education" className={`header-links ${location.pathname === "/education" ? "active-header" : ""}`}>Education</Link>
+                <Link to="/projects" className={`header-links ${location.pathname === "/projects" ? "active-header" : ""}`}>Projects</Link>
             </nav>
 
         </header>
