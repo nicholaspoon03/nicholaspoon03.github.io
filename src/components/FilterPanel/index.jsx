a71660f1efe3d5c64278247f4c2fc602c7584f95
nicholaspoon03 <npoon2003@gmail.com>
2025-07-27 16:41:45 -0400

    Fix FilterPanel responsive grid size

--- src/components/FilterPanel/index.jsx
+++ src/components/FilterPanel/index.jsx
@@ -3,19 +3,21 @@ import "./style.css";
 
 function FilterPanel({ expanded, activeFilters, filterList, onFilterClick, onDropdownClick, onClearClick }) {
     return (
-        <div className="page-layout">
-            <div id="filter-panel-controls" className="row">
-                <button className="outlined-button row chevron" onClick={onDropdownClick}>
-                    <p>Filter</p>
-                    { expanded ? <i className="bi bi-chevron-up"></i> : <i className="bi bi-chevron-down"></i> }
-                </button>
-
-                { expanded ?
-                    <button className="outlined-button" onClick={onClearClick}>
-                        <p>Clear</p>
+        <div id="filter-panel-div">
+            <div className="page-layout">
+                <div id="filter-panel-controls" className="row">
+                    <button className="outlined-button row chevron" onClick={onDropdownClick}>
+                        <p>Filter</p>
+                        { expanded ? <i className="bi bi-chevron-up"></i> : <i className="bi bi-chevron-down"></i> }
                     </button>
-                    : ""
-                }
+
+                    { expanded ?
+                        <button className="outlined-button" onClick={onClearClick}>
+                            <p>Clear</p>
+                        </button>
+                        : ""
+                    }
+                </div>
             </div>
 
             { expanded ? 
